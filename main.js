4949c53bd2c52faf58855ecd8f7543157f1eefdc
IuriSan <santanna.iuri@gmail.com>
2018-01-20 20:38:17 -0200

    Refactor code

--- main.js
+++ main.js
@@ -12,45 +12,42 @@ const INTprojectsIds = [
 
 // Get 24hs ago date in ISO-8601 format
 let yesterday = new Date();
-yesterday.setDate(yesterday.getDate() - 7); // today - 1 day
+yesterday.setDate(yesterday.getDate() - 1); // now less 1 day
 yesterday = yesterday.toISOString();
 
-const getCompletedTasks = projectsIdsArray => {
-    projectsIdsArray.forEach(projectId => {
-        const getOptions = {
-            url: `https://app.asana.com/api/1.0/projects/${projectId}/tasks?opt_fields=name,projects,completed,custom_fields&completed_since=${encodeURIComponent(yesterday)}`,
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer 0/34784122c7fc335cb34435c5682bddee'// Reminder Bot personal access token
-            }
-        };
-        // Fetch yesterday's tasks
-        request(getOptions, function (error, response, body) {
-            if (error) {
-                console.error('error:', error);
-            } else {
-                const data = JSON.parse(body).data;
-                for(let task in data) {
-                    if(data[task].completed && data[task].custom_fields) {
-                        let isMissingTimeSpent = data[task].custom_fields.some(field => {
-                            return field.name === 'Time spent' && field.number_value === null;
-                        });
-                        let isTamTask = data[task].name.indexOf('[TAM]') > -1;
-                        let isFromOtherTeam = data[task].projects.some(project => {
-                            return INTprojectsIds.indexOf(project.id) === -1;
-                        });
-
-                        if (isMissingTimeSpent && !isTamTask && !isFromOtherTeam) {
-                            postTimeSpentReminder(data[task]);
-                        }
+INTprojectsIds.forEach(projectId => {
+    const getOptions = {
+        url: `https://app.asana.com/api/1.0/projects/${projectId}/tasks?opt_fields=name,projects,completed,custom_fields&completed_since=${encodeURIComponent(yesterday)}`,
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer 0/34784122c7fc335cb34435c5682bddee'// Reminder Bot personal access token
+        }
+    };
+    // Fetch yesterday's tasks
+    request(getOptions, function (error, response, body) {
+        if (error) {
+            console.error(`Error fetching tasks for project id ${projectId}:`, error);
+        } else {
+            const data = JSON.parse(body).data;
+            data.forEach(task => {
+                if(task.completed && task.custom_fields) {
+                    let isMissingTimeSpent = task.custom_fields.some(field => {
+                        return field.name === 'Time spent' && field.number_value === null;
+                    });
+                    let isTamTask = task.name.indexOf('[TAM]') > -1;
+                    let isFromOtherTeam = task.projects.some(project => {
+                        return INTprojectsIds.indexOf(project.id) === -1;
+                    });
+                    if (isMissingTimeSpent && !isTamTask && !isFromOtherTeam) {
+                        postTimeSpentReminder(task);
                     }
                 }
-            }
-        });
+            });
+        }
     });
-};
+});
 
-const postTimeSpentReminder = task => {
+function postTimeSpentReminder(task) {
     const postOptions = {
         url: `https://app.asana.com/api/1.0/tasks/${task.id}/stories`,
         method: 'POST',
@@ -63,10 +60,9 @@ const postTimeSpentReminder = task => {
             }
         }
     };
-    //post reminder comment
     request(postOptions, function (error, response, body) {
-        if (error || body['errors']) {
-            console.error('error:', body['errors']);
+        if (body['errors']) {
+            console.error(`Error posting reminder for task id ${task.id}:`, body['errors']);
         } else {
             console.log('Reminder succesfully posted');
             console.log('task name:', task.name);
@@ -74,5 +70,3 @@ const postTimeSpentReminder = task => {
         }
     });
 };
-
-getCompletedTasks(INTprojectsIds);
